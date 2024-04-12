2eef9f43c2fa9d89fd56f90be1571c78436c9f94
Faris Abulula <abululb02@gmail.com>
2024-04-12 16:49:18 +0100

    date function done

--- days_api/app.py
+++ days_api/app.py
@@ -9,11 +9,14 @@ from flask import Flask, Response, request, jsonify
 from date_functions import (convert_to_datetime, get_day_of_week_on,
                             get_days_between, get_current_age)
 
+import pysnooper
+
 app_history = []
 
 app = Flask(__name__)
 
 
+@app.route("/history", methods=["GET", "DELETE"])
 def add_to_history(current_request):
     """Adds a route to the app history."""
     app_history.append({
@@ -26,7 +29,31 @@ def add_to_history(current_request):
 @app.get("/")
 def index():
     """Returns an API welcome messsage."""
-    return jsonify({ "message": "Welcome to the Days API." })
+    return jsonify({"message": "Welcome to the Days API."})
+
+
+@app.route("/between", methods=["POST"])
+@pysnooper.snoop()
+def between():
+    data = request.get_json()
+    if "first" not in data.keys() or "last" not in data.keys():
+        return {"error": "Missing required data."}, 400
+
+    first = data["first"]
+    last = data["last"]
+    if isinstance(first, datetime) or isinstance(last, datetime):
+        return get_days_between(first, last)
+    return {"error": "Unable to convert value to datetime."}, 400
+
+
+@app.route("/weekday", methods=["POST"])
+def weekday():
+    ...
+
+
+@app.route("/current_age", methods=["GET"])
+def current_age():
+    ...
 
 
 if __name__ == "__main__":
--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,16 +4,29 @@ from datetime import datetime, date
 
 
 def convert_to_datetime(date_val: str) -> datetime:
-    pass
+    """Change from sting to datetime"""
+    try:
+        return datetime.strptime(date_val, "%d.%m.%Y")
+    except ValueError as error:
+        raise ValueError("Unable to convert value to datetime.") from error
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    pass
+    """Find the number of days between the first and last date"""
+    if isinstance(first, datetime) and isinstance(last, datetime):
+        return (last - first).days
+    raise TypeError("Datetimes required.")
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
-    pass
+    """Find the name of the day the week is on"""
+    if isinstance(date_val, datetime):
+        return date_val.strftime("%A")
+    raise TypeError("Datetime required.")
 
 
 def get_current_age(birthdate: date) -> int:
-    pass
+    """Find your current age from your birthday"""
+    if isinstance(birthdate, date):
+        return int((date.today() - birthdate).days//365.25)
+    raise TypeError("Date required.")
